e3094e50ded6f464b10ec5524603881dd03e72a9
Michael Dreves <96418659+dr3v3s@users.noreply.github.com>
2025-10-30 21:50:17 +0100

    Add options.lua: general Neovim options

---
+++ lua/config/options.lua
@@ -0,0 +1,10 @@
+-- General Neovim options
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
